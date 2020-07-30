44bc7045b74107fad7d14303c349e92666570893
SWERecker <SWERecker@users.noreply.github.com>
2020-07-30 20:38:13 +0800

    update

--- edit.py
+++ edit.py
@@ -1,12 +1,15 @@
 import json
 to_del_list = []
-with open('song_list.json', 'r', encoding='utf-8') as song_file:
-	dict_data = json.load(song_file)
+with open('new_song_list_comp.json', 'r', encoding='utf-8') as song_file:
+    dict_data = json.load(song_file)
+    
 for index in range(len(dict_data)):
-		dict_data[index]['id'] = index
-		diff = int(dict_data[index].get('diff'))
-		dict_data[index]['diff'] = diff
+    if dict_data[index].get('name') == '':
+        print(index)
+    dict_data[index]['id'] = index
+    diff = int(dict_data[index].get('diff'))
+    dict_data[index]['diff'] = diff
 
 
-with open('new_song_list.json', 'w', encoding='utf-8') as new_song_file:
-	new_song_file.write(json.dumps(dict_data, ensure_ascii=False, indent=4))
+with open('new_song_list_comp.json', 'w', encoding='utf-8') as new_song_file:
+    new_song_file.write(json.dumps(dict_data, ensure_ascii=False, indent=4))
--- view.py
+++ view.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 
 @app.route('/random_song', methods=['post'])
 def get_ss():
-    diff_fil_result = []
-    band_fil_result = []
-    type_fil_result = []
-
+    result = []
     diff_filter = request.json.get('diff')
     app.logger.info(diff_filter)
     band_filter = request.json.get('band')
@@ -18,26 +15,23 @@ def get_ss():
     app.logger.info(type_filter)
     song_type = request.json.get('c_type')
 
-    if song_type == 'normal':
-        song_file = 'new_song_list.json'
-    else:
+    if song_type == 'competitive':
         song_file = 'new_song_list_comp.json'
+    else:
+        song_file = 'new_song_list.json'
 
     with open(song_file, 'r', encoding='utf-8') as song_file:
         dict_data = json.load(song_file)
         for i in range(len(dict_data)):
-            if dict_data[i]['diff'] in diff_filter:
-                diff_fil_result.append(dict_data[i])
-        for i in range(len(dict_data)):
-            if dict_data[i]['band'] in band_filter:
-                band_fil_result.append(dict_data[i])
-        for i in range(len(dict_data)):
-            if dict_data[i]['type'] in type_filter:
-                type_fil_result.append(dict_data[i])
-
-        temp = [val for val in diff_fil_result if val in band_fil_result]
-        result = [val for val in temp if val in type_fil_result]
-        return jsonify(random.choice(result))
+            if dict_data[i]['diff'] in diff_filter \
+                    and dict_data[i]['band'] in band_filter \
+                    and dict_data[i]['type'] in type_filter:
+                result.append(dict_data[i])
+        if not result:
+            return jsonify({"msg": "error"})
+        random_song = random.choice(result)
+        random_song["msg"] = "ok"
+        return jsonify(random_song)
 
 
-app.run(host='0.0.0.0', port=8802, debug=True)
+app.run(host='0.0.0.0', port=8802, debug=True, threaded=True)
